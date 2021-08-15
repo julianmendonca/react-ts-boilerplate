3c7d540a6f4168d7b00251491beb75922b72b47b
julianmcapicua <julian@capicua.com.uy>
2021-08-14 22:27:42 -0300

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+   env: {
+      browser: true,
+      node: true,
+      es2021: true,
+   },
+   extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+   parser: '@typescript-eslint/parser',
+   parserOptions: {
+      ecmaFeatures: {
+         jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+   },
+   settings: {
+      'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+   },
+   plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
+   rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-unused-vars': [
+         'error',
+         { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+      ],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'prettier/prettier': [
+         'error',
+         {
+            singleQuote: true,
+            jsxSingleQuote: true,
+            parser: 'flow',
+            tabWidth: 3,
+            semi: false,
+         },
+      ],
+   },
+}
